95b870068eb1b40238aac76192982c25d46368f9
Gonzalo Casanova <gcasanova@frba.utn.edu.ar>
2022-04-17 17:32:49 -0300

    prueba

--- git.sh
+++ git.sh
@@ -1,6 +1,13 @@
-COMMIT=$1
+MENSAJE=$1
 RAMA=$2
+RAMADEV=develop
 git status
 git add .
-git commit -m "$COMMIT"
-git push origin $RAMA
+git commit -m "$MENSAJE"
+git checkout RAMADEV  
+git pull origin RAMADEV
+git checkout $RAMA
+git rebase RAMADEV
+git checkout RAMADEV
+git merge $RAMA
+git push origin RAMADEV
