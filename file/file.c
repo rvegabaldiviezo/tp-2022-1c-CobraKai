71174be42ad0e729cc3900c292cf06023e3d28c3
Gonzalo Casanova <gcasanova@frba.utn.edu.ar>
2022-04-20 00:06:28 -0300

    funcion leer_hasta reconoce EOF

--- file/file.c
+++ file/file.c
@@ -11,7 +11,7 @@
 char* leer_hasta(char caracter_de_paro, FILE* file) {
 	char caracter = fgetc(file);
 	char* cadena_guardada = string_new();
-	while (caracter != caracter_de_paro) {
+	while (caracter != caracter_de_paro && caracter != EOF) {
 		string_append_with_format(&cadena_guardada, "%c", caracter);
 		caracter = fgetc(file);
 	}
