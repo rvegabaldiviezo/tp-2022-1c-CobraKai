a8bc4866f6a88d292e61f5d031165aa3a7e2630e
Gonzalo Casanova <gcasanova@frba.utn.edu.ar>
2022-04-24 02:24:54 -0300

    valido conexion con el cliente

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,8 +1,15 @@
+/*
+ * Para correr desde terminal:
+ * 1. Compilo kernel.c
+ * 2. Me paro en tp-2022-1c-CobraKai/kernel
+ * 3. Escribo ./Debug/kernel
+ */
+
 #include "kernel.h"
 
 int main(void) {
 
-	logger = log_create("./kernel.log", "Kernel", 1, LOG_LEVEL_DEBUG);
+	t_log* logger = log_create("./kernel.log", "KERNEL", true, LOG_LEVEL_DEBUG);
 
 	int server_fd = iniciar_servidor();
 
@@ -10,12 +17,17 @@ int main(void) {
 
 	int cliente_fd = esperar_cliente(server_fd);
 
+	if (!conexion_exitosa(cliente_fd)) {
+		log_error(logger, "No se pudo establecer la conexion con el cliente, abortando proceso");
+		return EXIT_FAILURE;
+	}
+
+	// TODO: reemplazar lista por colas (ver commons/collections/queue.h), ver bien como implementar los posibles estados
+
 	t_list* lista;
 
 	while (1) {
-
 		int cod_op = recibir_operacion(cliente_fd);
-
 		switch (cod_op) {
 		case MENSAJE:
 			recibir_mensaje(cliente_fd);
@@ -33,9 +45,19 @@ int main(void) {
 			break;
 		}
 	}
+	terminar_programa(logger);
 	return EXIT_SUCCESS;
 }
 
 void iterator(char* value) {
+	t_log* logger = log_create("./kernel.log", "KERNEL", true, LOG_LEVEL_DEBUG);
 	log_info(logger,"%s", value);
+}
+
+bool conexion_exitosa(int cliente) {
+	return cliente != -1;
+}
+
+void terminar_programa(t_log* logger) {
+	log_destroy(logger);
 }
