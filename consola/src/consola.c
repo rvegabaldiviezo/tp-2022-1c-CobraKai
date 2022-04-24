248ed518f3ea6a09b619e64199d9d072c8b0cd14
Gonzalo Casanova <gcasanova@frba.utn.edu.ar>
2022-04-24 02:19:37 -0300

    cambios varios a la consola

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -1,39 +1,30 @@
-#include "consola.h"
-#include "commons/config.h"
-#include "commons/string.h"
-#include "commons/log.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include "file/file.h"
-#include "cliente/utils.h"
+/*
+ * Para correr desde terminal:
+ * 1. Compilo consola.c
+ * 2. Levanto el kernel (explicado en kernel.c)
+ * 3. Me paro en tp-2022-1c-CobraKai/consola
+ * 4. Escribo ./Debug/consola "src/consola.config" "tamaño"
+ */
+
+#include "consola.h" // Los includes necesarios van en consola.h, aca solo va este
 
 int main(int argc, char** argv) {
+	t_log* logger = log_create("./src/consola.log", "CONSOLA", true, LOG_LEVEL_INFO);
 
-	t_log* logger = iniciar_logger();
-	validarCantidadParametrosConsola(argc, logger);
-	char* ip;
-
-	t_config* config = config_create(argv[1]);
-
-	char* key = "IP_KERNEL";
-	int response = config_has_property(config,key);
-	//log_info(logger,"ENTRO");
-	if(response >0){
-		ip = config_get_string_value(config,key);
-		log_info(logger, ip);
-	} else {
-		log_error(logger,"Error");
+	if (!cantidad_parametros_correcta(argc)) {
+		log_error(logger, "Cantidad de parametros incorrecta, abortando...");
+		return EXIT_FAILURE;
 	}
-	//char* ip = config_get_string_value(config, "IP");
-	log_info(logger,"SALIO");
-	char* puerto = config_get_string_value(config, "PUERTO_KERNEL");
+
+	t_config* config = config_create(argv[1]); // RUTA: "src/consola.config"
+	char* ip = config_get_string_value(config, IP_KERNEL);
+	char* puerto = config_get_string_value(config, PUERTO_KERNEL);
 
 	int conexion = crear_conexion(ip, puerto);
 	t_paquete* paquete = armar_paquete();
 	enviar_paquete(paquete, conexion);
 
-
-	terminar_programa(conexion,logger,config);
+	terminar_programa(conexion, logger, config);
 
 	return EXIT_SUCCESS;
 }
@@ -41,43 +32,24 @@ int main(int argc, char** argv) {
 t_paquete* armar_paquete() {
 	char* instruccion;
 	t_paquete* paquete = crear_paquete();
-	FILE* pseudocodigo = fopen("./src/pseudocodigo.txt", "rt");
-	instruccion = leer_hasta(CARACTER_SALTO_DE_LINEA, pseudocodigo);
-	while(!feof(pseudocodigo)) {
-		agregar_a_paquete(paquete, instruccion, string_length(instruccion));
-		puts(instruccion); // Solo para verificar, borrar despues
-		// parsear_instrucciones() <-- Implementar
-		instruccion = leer_hasta(CARACTER_SALTO_DE_LINEA, pseudocodigo);
+	FILE* instrucciones = fopen("./src/pseudocodigo.txt", "rt");
+	instruccion = leer_hasta(CARACTER_SALTO_DE_LINEA, instrucciones);
+	while(!feof(instrucciones)) {
+		agregar_a_paquete(paquete, instruccion, string_length(instruccion) + 1);
+		instruccion = leer_hasta(CARACTER_SALTO_DE_LINEA, instrucciones);
 	}
-	fclose(pseudocodigo);
-	//free(instruccion);
+	fclose(instrucciones);
+	free(instruccion);
 	return paquete;
 }
 
-void validarCantidadParametrosConsola(int argc, t_log* logger){
-	if(argc != 3) {
-		puts("CANTIDAD DE PARAMETROS INCORRECTA");
-		log_error(logger, "CANTIDAD DE PARAMETROS INCORRECTA");
-		exit(EXIT_FAILURE);
-	}
+bool cantidad_parametros_correcta(int cantidad) {
+	return cantidad == 3;
 }
 
-t_log* iniciar_logger(void)
-{
-	t_log* nuevo_logger;
-
-	nuevo_logger = log_create("./src/consola.log","CONSOLA", true, LOG_LEVEL_INFO);
-
-	return nuevo_logger;
-}
-
-//void terminar_programa(int conexion, t_log* logger, t_config* config)
-void terminar_programa(int conexion, t_log* logger, t_config* config)
-{
-	log_destroy(logger);
+void terminar_programa(int conexion, t_log* logger, t_config* config) {
 	config_destroy(config);
+	log_destroy(logger);
 	liberar_conexion(conexion);
 }
-
-
 
--- consola/src/consola.h
+++ consola/src/consola.h
@@ -1,10 +1,3 @@
-/*
- * consola.h
- *
- *  Created on: 17 abr. 2022
- *      Author: utnso
- */
-
 #ifndef CONSOLA_H_
 #define CONSOLA_H_
 
@@ -14,14 +7,19 @@
 #include "commons/string.h"
 #include "commons/config.h"
 #include "commons/log.h"
-
+#include "file/file.h"
 
 #define CARACTER_SALTO_DE_LINEA '\n'
+#define IP_KERNEL "IP_KERNEL"
+#define PUERTO_KERNEL "PUERTO_KERNEL"
+
+typedef struct {
+	char* instruccion;
+	int parametros[2];
+} t_instruccion; // Para no enviar instrucciones como string, igual consultar
 
 t_paquete* armar_paquete();
-char* leer_hasta(char caracter_de_paro, FILE* file);
-void validarCantidadParametrosConsola(int argc, t_log* logger);
-void terminar_programa(int conexion,t_log* logger,t_config* config);
-t_log* iniciar_logger(void);
+bool cantidad_parametros_correcta(int argc);
+void terminar_programa(int conexion, t_log* logger, t_config* config);
 
 #endif /* CONSOLA_H_ */
