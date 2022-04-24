41d4cc2371c911fda6da9538e2f76f4b218b0ba6
Gonzalo Casanova <gcasanova@frba.utn.edu.ar>
2022-04-24 02:25:11 -0300

    estructura pcb

--- kernel/src/kernel.h
+++ kernel/src/kernel.h
@@ -1,12 +1,25 @@
-#ifndef SERVER_H_
-#define SERVER_H_
+#ifndef KERNEL_H_
+#define KERNEL_H_
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <commons/log.h>
+#include <commons/collections/queue.h>
 #include "./server/utils.h"
 
-void iterator(char* value);
 
-#endif /* SERVER_H_ */
+typedef struct {
+	pid_t id;
+	unsigned int tamanio_proceso;
+	unsigned int program_counter;
+	unsigned int tablas_paginas;
+	unsigned int estimacion_rafaga;
+} t_pcb;
+
+void iterator(char* value);
+bool conexion_exitosa(int);
+void terminar_programa(t_log*);
+
+#endif /* KERNEL_H_ */
