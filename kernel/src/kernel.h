208a34f39c21dd777a657085bf9ec990cf09f1fa
Gonzalo Casanova <gcasanova@frba.utn.edu.ar>
2022-06-03 17:13:06 -0300

    cambio de archivo estructuras de proceso y pcb

--- kernel/src/kernel.h
+++ kernel/src/kernel.h
@@ -1,7 +1,9 @@
 #ifndef KERNEL_H_
 #define KERNEL_H_
 
+#include "utils_kernel/utils_kernel.h"
 #include <pthread.h>
+#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,7 +12,6 @@
 #include <commons/collections/queue.h>
 #include <commons/string.h>
 #include <commons/config.h>
-#include "utils_kernel/utils_kernel.h"
 
 #define PATH_CONFIG "src/kernel.config"
 #define PATH_LOG "./kernel.log"
@@ -24,17 +25,33 @@
 #define PUERTO_CPU "PUERTO_CPU"
 #define ALGORITMO_PLANIFICACION "ALGORITMO_PLANIFICACION"
 
-// Operaciones con consola
-enum {
-	LISTA_DE_INSTRUCCIONES = 1,
-	RESPUESTA_EXITO,
-	ERROR = -1
-};
+typedef struct {
+	pid_t id;
+	unsigned int tamanio_proceso;
+	t_list* instrucciones;
+	unsigned int program_counter;
+	unsigned int tablas_paginas;
+	unsigned int estimacion_rafaga;
+} t_pcb;
 
-// Operaciones con memoria
+typedef struct {
+	t_pcb pcb;
+	int socket;
+} t_proceso;
+
 enum {
-	TABLA_PAGINAS_PRIMER_NIVEL = 1
-};
+	LISTA_DE_INSTRUCCIONES = 1, // kernel crea un proceso a partir de las instrucciones recibidas y de ser posible lo asigna a la cola ready
+	RESPUESTA_EXITO, // al finalizar el proceso kernel envía la respuesta a la consola que lo haya solicitado
+	ERROR = -1
+} operaciones_consola;
+
+enum {
+	TABLA_PAGINAS_PRIMER_NIVEL = 1 // memoria crea las tablas de paginas para el proceso y devuelve el numero de la tabla de nivel uno
+} operaciones_memoria;
+
+enum {
+	REPLANIFICACION = 100 // se envía a traves de la conexion INTERRUPT: cpu corta la ejecucion en curso y devuelve el proceso con el pcb actualizado y la estimacion restante
+} operaciones_cpu;
 
 void iterator(char* value);
 bool conexion_exitosa(int);
@@ -45,7 +62,17 @@ void terminar_programa();
 //int solicitar_numero_de_tabla(int);
 bool numero_de_tabla_valido(int);
 void inicializar_colas();
+void inicializar_semaforos();
 int atender_consola();
 void planificar_srt();
+void * list_pop(t_list*);
+t_proceso* menor_tiempo_restante(t_proceso*, t_proceso*);
+t_proceso* crear_proceso(void);
+t_pcb crear_pcb();
+t_list* recibir_instrucciones(int socket_cliente);
+t_proceso* recibir_proceso(int socket_cliente);
+void destruir_proceso(t_proceso*);
+t_proceso* lista_mas_corta(t_proceso*, t_proceso*);
+bool repetido(t_proceso* p1, t_proceso* p2);
 
 #endif /* KERNEL_H_ */
