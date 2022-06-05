e8c5db578c243d05d8235c2870381c8224b5019a
Gonzalo Casanova <gcasanova@frba.utn.edu.ar>
2022-06-05 02:14:33 -0300

    ejecutables de cpu

--- cpu/Debug/sources.mk
+++ cpu/Debug/sources.mk
@@ -14,8 +14,4 @@ C_DEPS :=
 # Every subdirectory with source files must be described here
 SUBDIRS := \
 src \
-<<<<<<< HEAD
-src/utils_cpu \
-=======
->>>>>>> b6da311d0fe5dcc6601e756f50d85b7f316c465c
 
